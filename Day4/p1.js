468aae18ffe4f49e95bc58c5749fbdabe28c4997
unknown <kieugiangg509@gmail.com>
2022-03-27 01:07:52 +0700

    day4

---
+++ Day4/p1.js
@@ -0,0 +1,27 @@
+//destructuring
+    // const courses = ["HTML", "CSS", "JS", "React"];
+   //C1// // console.log(courses[2]);
+    //C2// // const [a, b, c, d] = courses;
+    // console.log(d);
+
+//rest parameters
+    // const courses = ["HTML", "CSS", "JS", "React"];
+    // const [a, ...rest] = courses;
+    // console.log(rest);
+
+//spread
+    // function a(...rest) {
+    //     console.log(rest);
+    // }
+    // a("Kiều Giang");
+
+    //--nối hàm--//
+    // const courses1 = ["HTML", "CSS"];
+    // const courses2 = ["JS", "React"];
+    
+    // // c1: 
+    // const newCourses = courses1.concat(courses2); 
+    // //c2:
+    // const newCourses1 = [...courses1, ...courses2];
+    // console.log(newCourses);
+    // console.log(newCourses1);
