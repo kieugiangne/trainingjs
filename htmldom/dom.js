e93a16e8210105c93341eef6a397494da3ca6e57
unknown <kieugiangg509@gmail.com>
2022-04-14 00:06:53 +0700

    dom

---
+++ htmldom/dom.js
@@ -0,0 +1,10 @@
+var titleNode = document.getElementById('title');
+console.log(titleNode);
+
+var titleNodes = document.getElementsByClassName('tile');
+console.log(titleNodes);
+
+var titleElement= document.querySelector('h1');
+titleElement.className = 'alo';
+titleElement.setAttribute('href', 'aloha');
+console.log(titleElement.getAttribute('class'));
