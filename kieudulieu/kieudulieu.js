26131937f5bbe6710c616aa0bb66c1f22dcbb3ea
unknown <kieugiangg509@gmail.com>
2022-04-19 11:18:31 +0700

    kieudulieu

---
+++ kieudulieu/kieudulieu.js
@@ -0,0 +1,31 @@
+//Number
+var x = 20;
+var y = 67.87;
+console.log(x + y);
+
+//String
+var x = "Hoang"
+var y = "Thi Kieu"
+var z = "Giang"
+console.log(x + " " + y + " " + z + " 20it8");
+
+//Boolean
+var m = 9;
+var n = 8;
+console.log(m < n);
+
+//Null
+var a = null; 
+console.log(a); 
+
+//Underfined
+var t; 
+console.log(t); 
+
+//Object
+var canhan = {
+    ten: 'Giang', 
+    masinhvien: '20it365',
+    tuoi:18
+    };
+console.log(canhan.tuoi);
